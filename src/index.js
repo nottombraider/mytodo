425b57693518bd7f08567dab911344761d2468dd
nottombraider <larysa.yavorska@outlook.com>
2019-12-03 23:15:24 +0100

    MYT-4 --create TODO App class --add to do

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('hello world');
+class App {
+  constructor() {
+    this.container = document.getElementById('app');
+  }
+
+  addToDo(task) {
+    const item = document.createElement('li');
+    const checkbox = document.createElement('input');
+
+    item.textContent = task.text;
+    checkbox.type = 'checkbox';
+    checkbox.checked = task.done;
+
+    item.append(checkbox);
+    this.container.append(item);
+  }
+}
+
+const myToDo = new App();
+myToDo.addToDo({
+  text: 'buy bred',
+  done: false,
+});
+console.log(myToDo);
