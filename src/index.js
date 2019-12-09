9876b9373bca00a82416115c95ec0a9ca50fd580
nottombraider <larysa.yavorska@outlook.com>
2019-12-09 21:21:06 +0100

    MYT-8 --create task lit item move to handler + fix context

--- src/index.js
+++ src/index.js
@@ -1,31 +1,4 @@
-const createTaskListItemMarkup = (task) => {
-  const itemContainer = document.createElement('li');
-  const itemTextBox = document.createElement('input');
-  const checkbox = document.createElement('input');
-
-  itemContainer.classList.add('task-item', 'button-line', 'flex');
-  itemTextBox.classList.add('input-task-field');
-
-  itemTextBox.value = task.text;
-  checkbox.type = 'checkbox';
-  checkbox.classList.add('task-item-checkbox');
-  checkbox.checked = task.done;
-
-  checkbox.addEventListener('change', (event) => {
-    const index = this.taskList.indexOf(task);
-    const activeTask = this.taskList[index];
-
-    activeTask.done = event.target.checked;
-  });
-  itemTextBox.addEventListener('input', (event) => {
-    // eslint-disable-next-line no-param-reassign
-    task.text += event.data;
-  });
-
-  itemContainer.append(itemTextBox);
-  itemContainer.append(checkbox);
-  this.taskListContainer.append(itemContainer);
-};
+import { createTaskListItemMarkup } from './handlers';
 
 class App {
   constructor() {
@@ -47,6 +20,7 @@ class App {
         };
         this.addToDo(task);
       } else {
+        // eslint-disable-next-line no-alert
         alert('To Do cannot be empty');
       }
     });
@@ -97,7 +71,7 @@ class App {
     } else {
       this.buttonDeleteCompleted.classList.add('hidden');
     }
-    this.taskList.forEach(createTaskListItemMarkup);
+    this.taskList.forEach(createTaskListItemMarkup(this));
     this.inputTaskField.value = '';
   }
 }
