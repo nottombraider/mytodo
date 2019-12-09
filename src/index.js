0e59598a156420622b37ff548b4d7ace02c7a607
nottombraider <larysa.yavorska@outlook.com>
2019-12-09 21:02:02 +0100

    MYT-8 --move creating task list item markup to separate function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+const createTaskListItemMarkup = (task) => {
+  const itemContainer = document.createElement('li');
+  const itemTextBox = document.createElement('input');
+  const checkbox = document.createElement('input');
+
+  itemContainer.classList.add('task-item', 'button-line', 'flex');
+  itemTextBox.classList.add('input-task-field');
+
+  itemTextBox.value = task.text;
+  checkbox.type = 'checkbox';
+  checkbox.classList.add('task-item-checkbox');
+  checkbox.checked = task.done;
+
+  checkbox.addEventListener('change', (event) => {
+    const index = this.taskList.indexOf(task);
+    const activeTask = this.taskList[index];
+
+    activeTask.done = event.target.checked;
+  });
+  itemTextBox.addEventListener('input', (event) => {
+    // eslint-disable-next-line no-param-reassign
+    task.text += event.data;
+  });
+
+  itemContainer.append(itemTextBox);
+  itemContainer.append(checkbox);
+  this.taskListContainer.append(itemContainer);
+};
+
 class App {
   constructor() {
     this.container = document.getElementById('app');
@@ -68,34 +97,7 @@ class App {
     } else {
       this.buttonDeleteCompleted.classList.add('hidden');
     }
-    this.taskList.forEach((task) => {
-      const itemContainer = document.createElement('li');
-      const itemTextBox = document.createElement('input');
-      const checkbox = document.createElement('input');
-
-      itemContainer.classList.add('task-item', 'button-line', 'flex');
-      itemTextBox.classList.add('input-task-field');
-
-      itemTextBox.value = task.text;
-      checkbox.type = 'checkbox';
-      checkbox.classList.add('task-item-checkbox');
-      checkbox.checked = task.done;
-
-      checkbox.addEventListener('change', (event) => {
-        const index = this.taskList.indexOf(task);
-        const activeTask = this.taskList[index];
-
-        activeTask.done = event.target.checked;
-      });
-      itemTextBox.addEventListener('input', (event) => {
-        // eslint-disable-next-line no-param-reassign
-        task.text += event.data;
-      });
-
-      itemContainer.append(itemTextBox);
-      itemContainer.append(checkbox);
-      this.taskListContainer.append(itemContainer);
-    });
+    this.taskList.forEach(createTaskListItemMarkup);
     this.inputTaskField.value = '';
   }
 }
