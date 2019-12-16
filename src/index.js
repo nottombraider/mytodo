7ab260be790c6e1a1c44f43d265af64db5725d43
lara <knopa9401@yandex.ru>
2019-12-16 22:14:27 +0100

    --add offline mode

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import { createTaskListItemMarkup } from './handlers';
 
+// offline mode
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js')
+      .then((reg) => {
+        console.log('Service worker registered.', reg);
+      });
+  });
+}
+
 class App {
   constructor() {
     this.container = document.getElementById('app');
---
+++ src/service-worker.js
@@ -0,0 +1,20 @@
+
+const cachedURLs = [
+  '/',
+  '/index.css',
+  '/index.js',
+];
+
+const cacheName = 'myToDoCache';
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('install', (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(cacheName)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(cachedURLs);
+      }),
+  );
+});
