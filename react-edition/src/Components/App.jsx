a1057a0e1f59b257645b9b51a30aea8ff9add11d
lara <knopa9401@yandex.ru>
2020-01-08 01:10:57 +0100

    --eslint

---
+++ react-edition/src/Components/App.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import TaskListItem from './TaskListItem';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [
+        {
+          id: 5,
+          text: 'call mom',
+          done: true,
+        },
+        {
+          id: 7,
+          text: 'learn to code',
+          done: false,
+        },
+        {
+          id: 10,
+          text: 'rest',
+          done: false,
+        },
+      ],
+    };
+
+    this.updateTaskStatus = this.updateTaskStatus.bind(this);
+  }
+
+  updateTaskStatus(id, status) {
+    const task = this.state.tasks.find((task) => task.id === id);
+
+    task.done = status;
+
+    const filteredTasks = this.state.tasks.filter((task) => task.id !== id);
+
+    filteredTasks.push(task);
+    this.setState({
+      tasks: filteredTasks,
+    });
+  }
+
+  render() {
+    const {
+      state: {
+        tasks,
+      },
+    } = this;
+
+    tasks.sort((taskA, taskB) => taskA.id - taskB.id);
+    return (
+      <ul>
+        {tasks.map((task) => (
+          <TaskListItem
+            task={task}
+            key={task.id}
+            toggleTaskStatus={this.updateTaskStatus}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default App;
---
+++ react-edition/src/Components/TaskListItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function TaskListItem(props) {
+  const {
+    task: { id, text, done },
+    toggleTaskStatus,
+  } = props;
+
+  const clickHandlerCheckbox = () => toggleTaskStatus(id, !done);
+
+  return (
+    <div>
+      <span className={done ? 'crossed' : ''}>{text}</span>
+      <input type="checkbox" defaultChecked={done} onClick={clickHandlerCheckbox} />
+    </div>
+  );
+}
+
+export default TaskListItem;
