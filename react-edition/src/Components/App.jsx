4fd2feb65c177f8a22be311d41cd9def51c84c80
lara <knopa9401@yandex.ru>
2020-01-09 22:15:13 +0100

    --create Main Layout component and update App component

--- react-edition/src/Components/App.jsx
+++ react-edition/src/Components/App.jsx
@@ -1,68 +1,8 @@
 import React from 'react';
-import TaskList from './TaskList';
+import MainLayout from './MainLayout';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tasks: [
-        {
-          id: 5,
-          text: 'call mom',
-          done: true,
-        },
-        {
-          id: 7,
-          text: 'learn to code',
-          done: false,
-        },
-        {
-          id: 10,
-          text: 'rest',
-          done: false,
-        },
-        {
-          id: 1,
-          text: 'do it',
-          done: false,
-        },
-      ],
-    };
-
-    this.updateTaskStatus = this.updateTaskStatus.bind(this);
-  }
-
-  updateTaskStatus(id, status) {
-    const {
-      state: {
-        tasks,
-      },
-    } = this;
-    const task = tasks.find((task) => task.id === id);
-
-    task.done = status;
-
-    const filteredTasks = tasks.filter((task) => task.id !== id);
-
-    filteredTasks.push(task);
-    this.setState({
-      tasks: filteredTasks,
-    });
-  }
-
-  render() {
-    const {
-      state: {
-        tasks,
-      },
-      updateTaskStatus,
-    } = this;
-
-    tasks.sort((taskA, taskB) => taskA.id - taskB.id);
-    return (
-      <TaskList tasks={tasks} updateTaskStatus={updateTaskStatus} />
-    );
-  }
-}
+const App = () => (
+  <MainLayout />
+);
 
 export default App;
---
+++ react-edition/src/Components/MainLayout.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import AddTask from './AddTask';
+import TaskList from './TaskList';
+
+class MainLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [
+        {
+          id: 5,
+          text: 'call mom',
+          done: true,
+        },
+        {
+          id: 7,
+          text: 'learn to code',
+          done: false,
+        },
+        {
+          id: 10,
+          text: 'rest',
+          done: false,
+        },
+        {
+          id: 1,
+          text: 'do it',
+          done: false,
+        },
+      ],
+    };
+    this.updateTaskStatus = this.updateTaskStatus.bind(this);
+  }
+
+  updateTaskStatus(id, status) {
+    const {
+      state: {
+        tasks,
+      },
+    } = this;
+    const task = tasks.find((task) => task.id === id);
+
+    task.done = status;
+
+    const filteredTasks = tasks.filter((task) => task.id !== id);
+
+    filteredTasks.push(task);
+    this.setState({
+      tasks: filteredTasks,
+    });
+  }
+
+  render() {
+    const {
+      state: {
+        tasks,
+      },
+      updateTaskStatus,
+    } = this;
+
+    tasks.sort((taskA, taskB) => taskA.id - taskB.id);
+    return (
+      <main>
+        <AddTask />
+        <TaskList tasks={tasks} updateTaskStatus={updateTaskStatus} />
+      </main>
+    );
+  }
+}
+
+export default MainLayout;
