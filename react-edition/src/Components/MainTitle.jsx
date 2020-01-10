04d76f5ef279f8f816eaf8f4f43e6b5264cf61b5
lara <knopa9401@yandex.ru>
2020-01-10 21:43:55 +0100

    --create styles for Main Title

--- react-edition/src/Components/MainTitle.jsx
+++ react-edition/src/Components/MainTitle.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const MainTitle = ({ title }) => (
-  <header className="flex">
-    <h1>{title}</h1>
-    <i className="gg-list" />
+  <header className="flex justify-center main-header">
+    <h1 className="main-header-title">{title}</h1>
+    <i className="gg-list gg-list-scale" />
   </header>
 );
 
