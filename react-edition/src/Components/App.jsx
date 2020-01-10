ccbbedd70455bb9c34a6f81097951b577b014b7f
lara <knopa9401@yandex.ru>
2020-01-10 23:51:05 +0100

    --create and add footer

--- react-edition/src/Components/App.jsx
+++ react-edition/src/Components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import MainTitle from './MainTitle';
 import MainLayout from './MainLayout';
+import MainFooter from './MainFooter';
 
 const App = () => (
   <>
     <MainTitle title="To Do List" />
     <MainLayout />
+    <MainFooter rights="@nottobraider" />
   </>
 );
 
---
+++ react-edition/src/Components/MainFooter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainFooter = ({ rights }) => (
+  <footer className="footer-text">
+    <span>
+      <strong>{rights}</strong>
+    </span>
+  </footer>
+);
+
+export default MainFooter;
