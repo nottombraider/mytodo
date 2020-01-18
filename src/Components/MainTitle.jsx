eabf7679b853668339aecd1dbad9071aba1e7418
lara <knopa9401@yandex.ru>
2020-01-18 22:41:06 +0100

    --update title

--- src/Components/MainTitle.jsx
+++ src/Components/MainTitle.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const MainTitle = ({ title }) => (
+const MainTitle = ({ title, titleDescription }) => (
   <header className="flex center wrap main-header">
-    <h1 className="main-header-title">{title}</h1>
+    <div className="title-text-container">
+      <h1 className="main-header-title">{title}</h1>
+      <span className="main-header-title-description">{titleDescription}</span>
+    </div>
     <i className="gg-list gg-scale" />
   </header>
 );
